80e199fdf3bf065576fbfdd54840fd80573bb810
Zi <zihao.wang92@gmail.com>
2016-09-28 18:06:57 +1000

    Add flip_controller source and make file into workspace

---
+++ src/modules/flip_controller/flip_controller.cpp
@@ -0,0 +1,28 @@
+#include <px4_config.h>
+#include <px4_defines.h>
+#include <px4_tasks.h>
+#include <px4_posix.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <poll.h>
+#include <drivers/drv_hrt.h>
+#include <arch/board/board.h>
+#include <uORB/uORB.h>
+#include <uORB/topics/vehicle_attitude_setpoint.h>
+#include <uORB/topics/vehicle_rates_setpoint.h>
+#include <uORB/topics/control_state.h>
+#include <uORB/topics/vehicle_control_mode.h>
+#include <uORB/topics/vehicle_status.h>
+/*
+ * flip_controller.cpp
+ *
+ *  Created on: 28Sep.,2016
+ *      Author: Zihao
+ */
+
+
+extern "C" __EXPORT int flip_controller(int argc, char *argv[]);
