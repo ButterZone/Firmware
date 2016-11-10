a34aceb75715ac45eff43b75cfec015853452bed
Zi <zihao.wang92@gmail.com>
2016-11-10 19:16:21 +1100

    Allow manual control in flip mode

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -3394,7 +3394,7 @@ set_control_mode()
 	switch (status.nav_state) {
 	case vehicle_status_s::NAVIGATION_STATE_FLIP:
 		control_mode.flag_control_flip_enabled = true;
-		control_mode.flag_control_manual_enabled = false;
+		control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
 		control_mode.flag_control_attitude_enabled = false;
